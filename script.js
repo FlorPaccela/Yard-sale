d3347aa2baccca5a153b9f7595436e88868f9b2e
florpaccela <florpaccela@gmail.com>
2022-12-19 11:34:14 -0300

    prueba

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-console.log ('Hola, mundo')
+const navEmail = document.querySelector ('#navEmail');
+const opcionesDesktop = document.querySelector ('.opcionesDesktop');
 
-const h1 = document.querySelector ('h1');
-//const p = document.querySelector ('p');
-//const title = Document.querySelector ('.title');
-//const pid = document.querySelector ('#pid');
-const input = document.querySelector ('input');
-const button = document.querySelector ('button');
+navEmail.addEventListener ('click', toggleopcionesDesktop);
 
-h1.innerHTML = "La cuenta de Pace";
-h1.innerText = "La cuenta de Pace";
-
-console.log (input.getAttribute ('type'));
-input.setAttribute ('type', 'verde');
-
-button.classList.remove ('buttonSecondary');
-button.classList.add ('otroboton');
-
-h1.classList.add ('otraclasemas');
-h1.classList.contains ('otraclasemas');
-
-input.value = '123';
+function toggleopcionesDesktop() {
+    opcionesDesktop.classList.toggle ('inactive');
+}
